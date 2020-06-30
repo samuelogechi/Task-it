574f1045634f643d607da5c908737e5db6420ea5
samuelogechi <67603488+samuelogechi@users.noreply.github.com>
2020-06-30 23:57:33 +0100

    update to tests

---
+++ app.test.js
@@ -0,0 +1,22 @@
+
+const newTask = require('./app');
+test("return new task", ()=>{
+    expect(newTask(e)).toBe(e.preventDefault());
+})
+
+test("remove a task", ()=>{
+    expect(removeTask(e)).toBe(e.target.classList.contains('remove-task')) {
+        e.target.parentElement.remove()};
+});
+
+test("getting tasks from storage", ()=>{
+    let tasks;
+  const tasksLS = localStorage.getItem('tasks');
+    expect(getTaskFromStorage).toBe(tasks = JSON.parse(tasksLS))
+})
+
+test("look through all tasks and delete", ()=>{
+    expect(tasksLS, index).toBe(tasks.splice(index, 1));
+});
+
+
